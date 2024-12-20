087f1bda364a2e5f353a4f828a08ccd7a6377673
Francesc Pérez <45601022+franzeszperez@users.noreply.github.com>
2024-12-20 23:43:16 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://franzezsperez.github.io',
   integrations: [tailwind(), icon()],
   output: "server",
   adapter: node({
